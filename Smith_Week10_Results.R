7eb0df78504f573fa1e57fb8125eff9cea6ed914
Daria Smith <dariajs@vt.edu>
2026-04-02 15:02:58 -0400

    updated

--- Smith_Week10_Results.R
+++ Smith_Week10_Results.R
@@ -4,22 +4,30 @@ cproj1 = read.csv("pheno_classdata_Jan29.csv")
 
 #HYPOTHESIS 1: Species Richness + Species predicts development stage
 
+#created a new dataframe to see just these three column so that I could tell what kind of 
 new_df <- cproj1[, c("SpeciesRichness", "Species", "FirstInitialGrowth")]
 new_df
 
+
+
 grichxgr = glm(FirstInitialGrowth ~ SpeciesRichness + Species, 
-               family = poisson, 
+               family = "Gamma", 
                data = cproj1)
 summary(grichxgr)
-
 # species riches and certain species (andge,poapr, and maybe luppe) can predict the rate of first initial growth
 
 
 
+# VPD and species seems to explain rate of leaves
+gVPDxgr = glm(FirstInitialGrowth ~ AvgVPD + Species, 
+               family = "Gamma", 
+               data = cproj1)
+summary(gVPDxgr)
 
-
-
-
+gVPDxle = glm(FirstLeaves ~ AvgVPD + Species, 
+              family = "Gamma", 
+              data = cproj1)
+summary(gVPDxle)
 
 
 
