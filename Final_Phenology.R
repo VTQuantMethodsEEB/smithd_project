4deab03c215fe73735923eba3096c3a52a233056
Daria Smith <dariajs@vt.edu>
2026-05-06 17:48:39 -0400

    update

--- Final_Phenology.R
+++ Final_Phenology.R
@@ -6,7 +6,7 @@ library(ggplot2)
 install.packages("ggpubr")
 library(ggpubr)
 
-#using this package because the paper experiment is modeled after uses this for phenoology vs micro
+#
 install.packages("nlme")
 library(nlme)
 library(dplyr)
@@ -14,42 +14,76 @@ library(dplyr)
 ### In all analyses you want all of your variables to be either factors or numbers
 
 #Phenology
-ccproj$FirstInitialGrowth <- as.numeric(ccproj$FirstInitialGrowth)
-ccproj$FirstLeaves <- as.numeric(ccproj$FirstLeaves)
-ccproj$FirstFlower <- as.numeric(as.character(ccproj$FirstFlower))
-ccproj$FirstOpenFlower <- as.numeric(ccproj$FirstOpenFlower)
-ccproj$FirstFruit <- as.numeric(ccproj$FirstFruit)
-ccproj$FirstRipeFruit <- as.numeric(ccproj$FirstRipeFruit)
-ccproj$SpeciesRichness <- as.numeric(ccproj$SpeciesRichness)
+cproj1$FirstInitialGrowth <- as.numeric(cproj1$FirstInitialGrowth)
+cproj1$FirstLeaves <- as.numeric(cproj1$FirstLeaves)
+cproj1$FirstFlower <- as.numeric(as.character(cproj1$FirstFlower))
+cproj1$FirstOpenFlower <- as.numeric(cproj1$FirstOpenFlower)
+cproj1$FirstFruit <- as.numeric(cproj1$FirstFruit)
+cproj1$FirstRipeFruit <- as.numeric(cproj1$FirstRipeFruit)
+cproj1$SpeciesRichness <- as.numeric(cproj1$SpeciesRichness)
 
 #Microclimate
-ccproj$AvgVPD <- as.numeric(ccproj$AvgTempC)
-ccproj$AvgVPD <- as.numeric(ccproj$AvgVPD)
-ccproj$AvgRHperc <- as.numeric(ccproj$AvgRHperc)
+cproj1$AvgVPD <- as.numeric(cproj1$AvgTempC)
+cproj1$AvgVPD <- as.numeric(cproj1$AvgVPD)
+cproj1$AvgRHperc <- as.numeric(cproj1$AvgRHperc)
 
 
-# summary subsets with data that includes species who all emegred in week 1
-ccproj_nobro=subset(ccproj,ccproj$Species!="broin")
-ccproj_nosol=subset(ccproj,ccproj$Species!="solri") # use for initial growth and leaves
-ccproj_nobropoasollup <- ccproj %>% filter(!grepl("broin|poapr|solri|luppe", Species)) # use for flower
-ccproj_wandaggsch <- ccproj %>% filter(!grepl("broin|poapr|solri|luppe|lesca", Species)) # use for open flower
-ccproj_nobrosolsch <- ccproj %>% filter(!grepl("broin|solri|schsc", Species)) # use for fruit
-ccproj_nobrosollessch <- ccproj %>% filter(!grepl("broin|lesca|solri|schsc", Species)) # use for ripe fruit
+# summary subsets with data that excludes species who all emerged in week 1
+cproj1_nobro=subset(cproj1,cproj1$Species!="broin")
+cproj1_nosol=subset(cproj1,cproj1$Species!="solri") # use for initial growth and leaves
+cproj1_nobropoasollup <- cproj1 %>% filter(!grepl("broin|poapr|solri|luppe", Species)) # use for flower
+cproj1_wandaggsch <- cproj1 %>% filter(!grepl("broin|poapr|solri|luppe|lesca", Species)) # use for open flower
+cproj1_nobrosolsch <- cproj1 %>% filter(!grepl("broin|solri|schsc", Species)) # use for fruit
+cproj1_nobrosollessch <- cproj1 %>% filter(!grepl("broin|lesca|solri|schsc", Species)) # use for ripe fruit
 
 
-# VPD vs Phenology
-itemp <- lme(FirstInitialGrowth ~ AvgTempC + SpeciesRichness*Species, random = ~ 1 | Ring/Plot_Num, data = ccproj_nosol, na.action = na.omit)
+##### CODING BEGINS 
+
+Hypothesis
+# VPD vs Phenology + SpeciesRichness*Species
+ivpd <- lme(FirstInitialGrowth ~ AvgVPD + SpeciesRichness*Species, random = ~ 1 | Ring/Plot_Num, data = cproj1_nosol, na.action = na.omit)
+anova(ivpd)
+
+lvpd <- lme(FirstLeaves ~ AvgVPD + Species*SpeciesRichness, random = ~ 1 | Ring/Plot_Num, data = cproj1_nosol, na.action = na.omit)
+anova(lvpd)
+
+fvpd<- lme(FirstFlower ~ AvgVPD + Species*SpeciesRichness, random = ~ 1 | Ring/Plot_Num, data = cproj1_nobropoasollup, na.action = na.omit)
+anova(fvpd)
+
+frvpd <- lme( FirstFruit ~ AvgVPD + Species*SpeciesRichness, random = ~ 1 | Ring/Plot_Num, data = cproj1_nobrosolsch, na.action = na.omit)
+anova(frvpd)
+
+rvpd <- lme( FirstRipeFruit ~ AvgVPD + Species*SpeciesRichness, random = ~ 1 | Ring/Plot_Num, data = cproj1_nobrosollessch, na.action = na.omit)
+anova(rvpd)
+
+# Temp vs Phenology + SpeciesRichness*Species
+itemp <- lme(FirstInitialGrowth ~ AvgTempC + SpeciesRichness*Species, random = ~ 1 | Ring/Plot_Num, data = cproj1_nosol, na.action = na.omit)
 anova(itemp)
 
-ltemp <- lme(FirstLeaves ~ AvgVPD + Species*SpeciesRichness, random = ~ 1 | Ring/Plot_Num, data = ccproj_nosol, na.action = na.omit)
+ltemp <- lme(FirstLeaves ~ AvgTempC + Species*SpeciesRichness, random = ~ 1 | Ring/Plot_Num, data = cproj1_nosol, na.action = na.omit)
 anova(ltemp)
 
-ftemp<- lme(FirstFlower ~ AvgTempC + Species*SpeciesRichness, random = ~ 1 | Ring/Plot_Num, data = ccproj_nobropoasollup, na.action = na.omit)
+ftemp<- lme(FirstFlower ~ AvgTempC + Species*SpeciesRichness, random = ~ 1 | Ring/Plot_Num, data = cproj1_nobropoasollup, na.action = na.omit)
 anova(ftemp)
 
-frtemp <- lme( FirstFruit ~ AvgTempC + Species*SpeciesRichness, random = ~ 1 | Ring/Plot_Num, data = ccproj_nobrosolsch, na.action = na.omit)
-anova(ftemp)
+frtemp <- lme( FirstFruit ~ AvgTempC + Species*SpeciesRichness, random = ~ 1 | Ring/Plot_Num, data = cproj1_nobrosolsch, na.action = na.omit)
+anova(frtemp)
 
-rtemp <- lme( FirstRipeFruit ~ AvgTempC + Species*SpeciesRichness, random = ~ 1 | Ring/Plot_Num, data = ccproj_nobrosollessch, na.action = na.omit)
+rtemp <- lme( FirstRipeFruit ~ AvgTempC + Species*SpeciesRichness, random = ~ 1 | Ring/Plot_Num, data = cproj1_nobrosollessch, na.action = na.omit)
 anova(rtemp)
 
+#Humidity vs vs Phenology + SpeciesRichness*Species
+ihum <- lme(FirstInitialGrowth ~ AvgRHperc + SpeciesRichness*Species, random = ~ 1 | Ring/Plot_Num, data = cproj1_nosol, na.action = na.omit)
+anova(ihum)
+
+lhum <- lme(FirstLeaves ~ AvgRHperc + Species*SpeciesRichness, random = ~ 1 | Ring/Plot_Num, data = cproj1_nosol, na.action = na.omit)
+anova(lhum)
+
+fhum <- lme(FirstFlower ~ AvgRHperc + Species*SpeciesRichness, random = ~ 1 | Ring/Plot_Num, data = cproj1_nobropoasollup, na.action = na.omit)
+anova(fhum)
+
+frhum <- lme( FirstFruit ~ AvgRHperc + Species*SpeciesRichness, random = ~ 1 | Ring/Plot_Num, data = cproj1_nobrosolsch, na.action = na.omit)
+anova(frhum)
+
+rhum <- lme( FirstRipeFruit ~ AvgRHperc + Species*SpeciesRichness, random = ~ 1 | Ring/Plot_Num, data = cproj1_nobrosollessch, na.action = na.omit)
+anova(rhum)
