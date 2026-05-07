0262a6538fa40df00e1fe3d0005ba4b4cedb58e7
Daria Smith <dariajs@vt.edu>
2026-05-06 21:34:24 -0400

    update

--- Final_Phenology.R
+++ Final_Phenology.R
@@ -123,6 +123,240 @@ aictab(fitListflower)
 aictab(fitListfruit)
 aictab(fitListripe)
 
+########### Figures
 
-#figures
+## pheno vs microclimate excluding bad ones inigr, leav, flow, oflow, frui, rfrui 
 
+inigr <- cproj1 %>% filter(!grepl("solri|andge|luppe|poapr", Species)) # initial growth use for which removes all that don't have enough data and all grew in the first weekinitial growth
+leav<- cproj1 %>% filter(!grepl("solri|andge|luppe|poapr", Species)) # leaves
+flow <- cproj1 %>% filter(!grepl("agrre|luppe|poapr|broin|solri", Species)) # flower
+oflow <- cproj1 %>% filter(!grepl("broin|poapr|solri|luppe|lesca", Species)) # open flower
+frui <- cproj1 %>% filter(!grepl("broin|solri|schsc|luppe", Species)) # fruit
+rfrui <- cproj1 %>% filter(!grepl("broin|lesca|solri|schsc", Species)) # ripe fruit
+
+#initial growth
+gxvpd <- ggplot( inigr, aes ( x = AvgVPD, y = FirstInitialGrowth, group = Species)) +
+  geom_point (aes(color = Species), size = 3) +
+  geom_smooth (aes(color = Species),method = "lm") +
+  labs(
+    x = "Avg Plot VPD",
+    y = "Week of First Initial Growth") +
+  theme_minimal() +
+  theme(
+    panel.background = element_rect(fill = "white", color = NA),
+    plot.background  = element_rect(fill = "white", color = NA)
+  )
+
+gxvpd
+
+gxtemp <- ggplot( inigr, aes ( x = AvgTempC, y = FirstInitialGrowth, group = Species)) +
+  geom_point (aes(color = Species), size = 3) +
+  geom_smooth (aes(color = Species),method = "lm") +
+  labs(
+    x = "Avg Plot Temp C",
+    y = "Week of First Initial Growth") +
+  theme_minimal() +
+  theme(
+    panel.background = element_rect(fill = "white", color = NA),
+    plot.background  = element_rect(fill = "white", color = NA)
+  )
+
+gxtemp
+
+gxhum <- ggplot( inigr, aes ( x = AvgRHperc, y = FirstInitialGrowth, color=Species)) +
+  geom_point (size = 3) +
+  geom_smooth (method = "lm") +
+  labs(
+    x = "Avg Plot Hum % ",
+    y = "Week of First Initial Growth") +
+  theme_minimal() +
+  theme(
+    panel.background = element_rect(fill = "white", color = NA),
+    plot.background  = element_rect(fill = "white", color = NA)
+  )
+
+gxhum
+
+#leaves
+lxvpd <- ggplot( leav, aes ( x = AvgVPD, y = FirstLeaves, color = Species)) +
+  geom_point (size = 3) +
+  geom_smooth (method = "lm") +
+  labs(
+    x = "Avg Plot VPD",
+    y = "Week of First Leaves") +
+  theme_minimal() +
+  theme(
+    panel.background = element_rect(fill = "white", color = NA),
+    plot.background  = element_rect(fill = "white", color = NA)
+  )
+
+lxvpd
+
+lxtemp <- ggplot( leav, aes ( x = AvgTempC, y = FirstLeaves, color = Species)) +
+  geom_point (size = 3) +
+  geom_smooth (method = "lm") +
+  labs(
+    x = "Avg Plot Temp C",
+    y = "Week of First Leave") +
+  theme_minimal() +
+  theme(
+    panel.background = element_rect(fill = "white", color = NA),
+    plot.background  = element_rect(fill = "white", color = NA)
+  )
+
+lxtemp
+
+lxhum <- ggplot( leav, aes ( x = AvgRHperc, y = FirstLeaves, color = Species)) +
+  geom_point (size = 3) +
+  geom_smooth (method = "lm") +
+  labs(
+    x = "Avg Plot Hum % ",
+    y = "Week of First Leaves") +
+  theme_minimal() +
+  theme(
+    panel.background = element_rect(fill = "white", color = NA),
+    plot.background  = element_rect(fill = "white", color = NA)
+  )
+
+lxhum
+
+## flower
+flxvpd <- ggplot( flow, aes ( x = AvgVPD, y = FirstFlower, color = Species)) +
+  geom_point (size = 3) +
+  geom_smooth (method = "lm") +
+  labs(
+    x = "Avg Plot VPD",
+    y = "Week of First Flower") +
+  theme_minimal() +
+  theme(
+    panel.background = element_rect(fill = "white", color = NA),
+    plot.background  = element_rect(fill = "white", color = NA)
+  )
+
+flxvpd
+
+flxtemp <- ggplot( flow, aes ( x = AvgTempC, y = FirstFlower, color = Species)) +
+  geom_point (size = 3) +
+  geom_smooth (method = "lm") +
+  labs(
+    x = "Avg Plot Temp C",
+    y = "Week of First Flower") +
+  theme_minimal() +
+  theme(
+    panel.background = element_rect(fill = "white", color = NA),
+    plot.background  = element_rect(fill = "white", color = NA)
+  )
+
+flxtemp
+
+flxhum <- ggplot( flow, aes ( x = AvgRHperc, y = FirstFlower, color = Species)) +
+  geom_point (size = 3) +
+  geom_smooth (method = "lm") +
+  labs(
+    x = "Avg Plot Hum % ",
+    y = "Week of First Flower") +
+  theme_minimal() +
+  theme(
+    panel.background = element_rect(fill = "white", color = NA),
+    plot.background  = element_rect(fill = "white", color = NA)
+  )
+
+flxhum
+
+####### Fruit
+frxvpd <- ggplot( frui, aes ( x = AvgVPD, y = FirstFruit, color = Species)) +
+  geom_point (size = 3) +
+  geom_smooth (method = "lm") +
+  labs(
+    x = "Avg Plot VPD",
+    y = "Week of First Fruit") +
+  theme_minimal() +
+  theme(
+    panel.background = element_rect(fill = "white", color = NA),
+    plot.background  = element_rect(fill = "white", color = NA)
+  )
+
+frxvpd
+
+frxtemp <- ggplot( frui, aes ( x = AvgTempC, y = FirstFruit, color = Species)) +
+  geom_point (size = 3) +
+  geom_smooth (method = "lm") +
+  labs(
+    x = "Avg Plot Temp C",
+    y = "Week of First Fruit") +
+  theme_minimal() +
+  theme(
+    panel.background = element_rect(fill = "white", color = NA),
+    plot.background  = element_rect(fill = "white", color = NA)
+  )
+
+frxtemp
+
+frxhum <- ggplot( frui, aes ( x = AvgRHperc, y = FirstFruit, color = Species)) +
+  geom_point (size = 3) +
+  geom_smooth (method = "lm") +
+  labs(
+    x = "Avg Plot Hum % ",
+    y = "Week of First Fruit") +
+  theme_minimal() +
+  theme(
+    panel.background = element_rect(fill = "white", color = NA),
+    plot.background  = element_rect(fill = "white", color = NA)
+  )
+
+frxhum
+
+####### Ripe Fruit
+rifxvpd <- ggplot( rfrui, aes ( x = AvgVPD, y = FirstRipeFruit, color = Species)) +
+  geom_point (size = 3) +
+  geom_smooth (method = "lm") +
+  labs(
+    x = "Avg Plot VPD",
+    y = "Week of First Ripe Fruit") +
+  theme_minimal() +
+  theme(
+    panel.background = element_rect(fill = "white", color = NA),
+    plot.background  = element_rect(fill = "white", color = NA)
+  )
+
+rifxvpd
+
+rifxtemp <- ggplot( rfrui, aes ( x = AvgTempC, y = FirstRipeFruit, color = Species)) +
+  geom_point (size = 3) +
+  geom_smooth (method = "lm") +
+  labs(
+    x = "Avg Plot Temp C",
+    y = "Week of First Ripe Fruit") +
+  theme_minimal() +
+  theme(
+    panel.background = element_rect(fill = "white", color = NA),
+    plot.background  = element_rect(fill = "white", color = NA)
+  )
+
+rifxtemp
+
+rifxhum <- ggplot( rfrui, aes ( x = AvgRHperc, y = FirstRipeFruit, color = Species)) +
+  geom_point (size = 3) +
+  geom_smooth (method = "lm") +
+  labs(
+    x = "Avg Plot Hum % ",
+    y = "Week of First Ripe Fruit") +
+  theme_minimal() +
+  theme(
+    panel.background = element_rect(fill = "white", color = NA),
+    plot.background  = element_rect(fill = "white", color = NA)
+  )
+
+rifxhum
+
+# make on figure per microclimate measurment
+
+library(patchwork)
+vpd <- gxvpd + lxvpd + flxvpd + frxvpd + rifxvpd  + plot_layout(nrow = 2, ncol = 3)
+vpd
+
+temp <- gxtemp + lxtemp + flxtemp + frxtemp + rifxtemp  + plot_layout(nrow = 2, ncol = 3)
+temp
+
+hum <- gxhum + lxhum + flxhum + frxhum + rifxhum  + plot_layout(nrow = 2, ncol = 3)
+hum
